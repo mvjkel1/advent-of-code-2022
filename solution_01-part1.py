704d3e85f36816d0b818167830b54953df4f9ba6
mvjkel1 <mvjkel9@gmail.com>
2022-12-10 14:52:33 +0100

    Solution 01 part 1

---
+++ solution_01-part1.py
@@ -0,0 +1,18 @@
+
+def solution():
+    with open("input") as file:
+        lines = [line.strip() for line in file]
+
+    tmp = 0
+    tmpList = []
+    for line in lines:
+        if line.strip() == "":
+            tmp = 0
+        else:
+            tmp += int(line.strip())
+            tmpList.append(tmp)
+
+    return(max(tmpList))
+
+if __name__ == '__main__':
+    print(solution())
